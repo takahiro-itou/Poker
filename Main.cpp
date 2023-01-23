3b9f589c645e82c14a25e5e88da081c39b45b8e4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-23 22:10:07 +0900

    リファクタリング

--- Main.cpp
+++ Main.cpp
@@ -22,23 +22,33 @@ showCards(const int buf[], const int R, std::ostream & os)
 
 int main(int argc, char * argv[])
 {
-    CombinationGenerator<6, 2>  comb;
+    CombinationGenerator<52, 7> comb;
 
     int counter = 0;
     int buf[MAX_BUF];
 
+    int total = 133784560;
     comb.resetGenerator();
     do {
         ++ counter;
         comb.getCurrent(buf);
 
+#if 0
         std::cout   <<  counter
                     <<  "\t";
         showCards(buf, 2, std::cout)    <<  std::endl;
-        if ( counter >= 32 ) {
-            break;
+#endif
+        if ( (counter & 65535) == 0 ) {
+            std::cerr   <<  counter <<  " / "
+                        <<  total
+                        <<  " ("
+                        <<  (counter * 100.0 / total)
+                        <<  " %)\r";
         }
     } while ( comb.generateNext() );
+
+    std::cerr   <<  counter
+                <<  "\nFinish."   <<  std::endl;
 
     return ( 0 );
 }
