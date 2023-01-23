f0f5e2ff6e277ad6eba28c246a2c5c1ece2776ad
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-23 21:12:17 +0900

    クラスを新規作成

---
+++ CombinationGenerator.h
@@ -0,0 +1,100 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Poker Hands Probability.  ---                  **
+**                                                                      **
+**          Copyright (C), 2023,      Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of CombinationGenerator class.
+**
+**      @file       CombinationGenerator.h
+**/
+
+#if !defined( POKER_HANDS_PROB_INCLUDED_COMBINATION_GENERATOR_H )
+#    define   POKER_HANDS_PROB_INCLUDED_COMBINATION_GENERATOR_H
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    CombinationGenerator  class.
+//
+/**
+**    組み合わせを生成するクラス。
+**/
+
+class  CombinationGenerator
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   CombinationGeneratorTest;
+};
+
+#endif
