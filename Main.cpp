913e16026da83727ed4cd2d69e32815ce5e74f2c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-28 11:14:11 +0900

    不要になったコードを削除

--- Main.cpp
+++ Main.cpp
@@ -9,26 +9,8 @@ exit 0;
 
 #include    <iostream>
 
-constexpr   int     MAX_BUF = 16;
-
-typedef     CombinationGenerator<52, 5>     CardPatternGenerator;
-typedef     CardPatternGenerator::Pattern   CardPatternBuffer;
-
-
-
-std::ostream  &
-showCards(const int buf[], const int R, std::ostream & os)
-{
-    for ( int i = 0; i < R; ++ i ) {
-        os  <<  buf[i]  <<  ",";
-    }
-    return ( os );
-}
-
-
 int main(int argc, char * argv[])
 {
     countHandPatterns<5>(std::cout, std::cerr);
-    return ( 0 );
     return ( 0 );
 }
