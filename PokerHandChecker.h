6a9729f0f89228f9e4234efb41b47716f48b9b6e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-28 10:47:56 +0900

    ヘッダファイルを新規作成

---
+++ PokerHandChecker.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Poker Hands Probability.  ---                  **
+**                                                                      **
+**          Copyright (C), 2023,      Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**
+**
+**      @file       PokerHandChecker.h
+**/
+
+#if !defined( POKER_HANDS_PROB_INCLUDED_POKER_HAND_CHECKER_H )
+#    define   POKER_HANDS_PROB_INCLUDED_POKER_HAND_CHECKER_H
+
+#endif
