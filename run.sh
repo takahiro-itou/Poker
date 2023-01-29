a5641d6abed62714d710bfbd9264db472958d0eb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-01-29 18:34:11 +0900

    実行結果

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 echo "---------- フロップ ----------"
 time ./Calc5.exe
 
+echo ""
 echo "----------  ターン  ----------"
 time ./Calc6.exe
 
+echo ""
 echo "----------  リバー  ----------"
 time ./Calc7.exe
