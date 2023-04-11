a157c0ba676d04c1126de1b5caff9987edf04674
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-04-11 23:53:38 +0900

    ファイルをコピー

---
+++ generator/RepeatedPermutationGenerator.h
@@ -0,0 +1,161 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Poker Hands Probability.  ---                  **
+**                                                                      **
+**          Copyright (C), 2023,      Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of PermutationGenerator class.
+**
+**      @file       PermutationGenerator.h
+**/
+
+#if !defined( POKER_HANDS_PROB_INCLUDED_PERMUTATION_GENERATOR_H )
+#    define   POKER_HANDS_PROB_INCLUDED_PERMUTATION_GENERATOR_H
+
+//  クラスの前方宣言。  //
+
+//========================================================================
+//
+//    PermutationGenerator  class.
+//
+/**
+**    順列を生成するクラス。
+**/
+
+template <int N, int R, int FIRST=1>
+class  PermutationGenerator
+{
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+public:
+
+    typedef     int     Pattern[R];
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    PermutationGenerator();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~PermutationGenerator();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   内部状態をリセットして最初のパターンに戻す。
+    **
+    **  @return     void.
+    **/
+    void
+    resetGenerator();
+
+    //----------------------------------------------------------------
+    /**   内部状態を更新して次のパターンを生成する。
+    **
+    **  @retval     true    次のパターンが存在する。
+    **  @retval     false   現在のパターンが最後。
+    **/
+    bool
+    generateNext();
+
+//========================================================================
+//
+//    Accessors.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   現在のパターンをコピーする。
+    **
+    **  @param[out] buf     結果を格納する変数。
+    **/
+    template <int K>
+    void
+    getCurrent(int  (& buf)[K])  const;
+
+    //----------------------------------------------------------------
+    /**   現在のパターンを取得する。
+    **
+    **/
+    const   Pattern  &
+    getCurrent()  const;
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+private:
+
+    /**   現在のパターン。  **/
+    int     m_buf[R];
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   PermutationGeneratorTest;
+};
+
+//  Implementation.
+#include    "PermutationGenerator.inl"
+
+#endif
---
+++ generator/RepeatedPermutationGenerator.inl
@@ -0,0 +1,149 @@
+﻿//  -*-  coding: utf-8-with-signature-unix; mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Poker Hands Probability.  ---                  **
+**                                                                      **
+**          Copyright (C), 2023,      Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of PermutationGenerator class.
+**
+**      @file       PermutationGenerator.inl
+**/
+
+#if !defined( POKER_HANDS_PROB_INCLUDED_PERMUTATION_GENERATOR_INL )
+#    define   POKER_HANDS_PROB_INCLUDED_PERMUTATION_GENERATOR_INL
+
+#if !defined( POKER_HANDS_PROB_INCLUDED_PERMUTATION_GENERATOR_H )
+#    include    "PermutationGenerator.h"
+#endif
+
+//========================================================================
+//
+//    PermutationGenerator  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+template <int N, int R, int FIRST>
+PermutationGenerator<N, R, FIRST>::PermutationGenerator()
+    : m_buf()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+template <int N, int R, int FIRST>
+PermutationGenerator<N, R, FIRST>::~PermutationGenerator()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//----------------------------------------------------------------
+//    内部状態をリセットして最初のパターンに戻す。
+//
+
+template <int N, int R, int FIRST>
+inline  void
+PermutationGenerator<N, R, FIRST>::resetGenerator()
+{
+    for ( int i = 0; i < R; ++ i ) {
+        this->m_buf[i]  = FIRST + i;
+    }
+}
+
+//----------------------------------------------------------------
+//    内部状態を更新して次のパターンを生成する。
+//
+
+template <int N, int R, int FIRST>
+inline  bool
+PermutationGenerator<N, R, FIRST>::generateNext()
+{
+    return ( false );
+}
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//----------------------------------------------------------------
+//    現在のパターンをコピーする。
+//
+
+template <int N, int R, int FIRST>
+template <int K>
+inline  void
+PermutationGenerator<N, R, FIRST>::getCurrent(int (& buf)[K])  const
+{
+    static_assert(R <= K, "The size of buffer is too small.");
+    for ( int i = 0; i < R; ++ i ) {
+        buf[i]  = this->m_buf[i];
+    }
+}
+
+//----------------------------------------------------------------
+//    現在のパターンを取得する。
+//
+
+template <int N, int R, int FIRST>
+inline  const   typename  PermutationGenerator<N, R, FIRST>::Pattern  &
+PermutationGenerator<N, R, FIRST>::getCurrent()  const
+{
+    return ( this->m_buf );
+}
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+#endif
